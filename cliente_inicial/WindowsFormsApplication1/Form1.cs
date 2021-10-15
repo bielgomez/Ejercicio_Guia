6656b0bd036ebf198d1d9eab915bc8295c0b154f
Biel Gómez <biel.gomez@estudiantat.upc.edu>
2021-10-15 19:37:01 +0200

    15/10/2021

---
+++ cliente_inicial/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+   
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress iPAddress = IPAddress.Parse("10.0.3.15");
+            IPAddress direc = iPAddress;
+            IPEndPoint ipep = new IPEndPoint(direc, 9080);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+             
+
+                if (Longitud.Checked)
+                {
+                    // Quiere saber la longitud
+                    string mensaje = "1/" + nombre.Text;
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    MessageBox.Show("La longitud de tu nombre es: " + mensaje);
+                }
+                else if (Altura.Checked)
+                {
+                    //Quiere saber si es alto o no
+                    string mensaje = "3/" + nombre.Text + "/" + alturaBox.Text;
+                    //Enviamos al servidor el nombre de la persona y su altura
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+                    //Procesamos la respuesta
+                    if (mensaje == "SI")
+                        MessageBox.Show(nombre.Text+", eres alto/a.");
+                    else
+                        MessageBox.Show(nombre.Text + ", eres bajo/a.");
+                }
+                else
+                {
+                    // Quiere saber si el nombre es bonito
+                    string mensaje = "2/" + nombre.Text;
+                    // Enviamos al servidor el nombre tecleado
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+
+                    if (mensaje == "SI")
+                        MessageBox.Show("Tu nombre ES bonito.");
+                    else
+                        MessageBox.Show("Tu nombre NO bonito. Lo siento.");
+
+
+                }
+             
+                // Se terminó el servicio. 
+                // Nos desconectamos
+                this.BackColor = Color.Gray;
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+
+
+
+            }
+            catch (SocketException )
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            } 
+
+          
+
+    
+          
+          
+
+        }
+
+   
+
+     
+    }
+}
